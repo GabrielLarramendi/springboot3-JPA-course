a6268983b35310501a3394307c2f8c746e4f6a8c
larramendi <larramendi.dev@gmail.com>
2023-01-26 17:56:03 -0300

    Project created

---
+++ src/main/java/com/larramendi/course/CourseApplication.java
@@ -0,0 +1,13 @@
+package com.larramendi.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  //Configura o projeto para que seja um projeto SpringBoot
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
