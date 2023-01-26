98ba2dffe5967421e53f075b4c45ce877bfb1181
larramendi <larramendi.dev@gmail.com>
2023-01-26 20:29:58 -0300

    Config. dependecias JPA e banco de dados H2 - Mapping entidade User (JPA) - Anotation para o banco de dados gerar automaticamente a ID e ela ser a chave primária da tabela

--- src/main/java/com/larramendi/course/entities/User.java
+++ src/main/java/com/larramendi/course/entities/User.java
@@ -1,10 +1,16 @@
 package com.larramendi.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user") //Como 'User' é uma palavra utilizada no dbH2, é necessário renomear o nome da tabela para evitar conflitos;
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
