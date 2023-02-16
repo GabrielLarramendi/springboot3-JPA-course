9ac43acfeb62ee940723db97aaa0fa7e0dfb4ea0
larramendi <larramendi.dev@gmail.com>
2023-02-16 15:14:24 -0300

    Order, Instant, ISO 8601 Pt. 2

---
+++ src/main/java/com/larramendi/course/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.larramendi.course.services;
+
+import com.larramendi.course.entities.Order;
+import com.larramendi.course.entities.User;
+import com.larramendi.course.repositories.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class OrderService {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    public List<Order> findAll() {
+        return orderRepository.findAll();
+    }
+
+    public Order findById(Long id) {
+        Optional<Order> obj = orderRepository.findById(id);
+        return obj.get();
+    }
+
+}
